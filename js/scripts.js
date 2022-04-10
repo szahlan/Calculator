f8f31158658347ebc47387a7984b65f5acf2a71c
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-09 17:01:22 -0700

    Remove pop-up dialogue boxes and display result directly on the webpage.

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,12 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+$(document).ready(function() { 
+  $("form#add").submit(function(event) {
+    event.preventDefault(); 
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
