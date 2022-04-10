207f8d895b2684c1a2f015051b7a6d23ca50ccf9
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-09 21:59:55 -0700

    Add branching to allow for performance of all four operations.

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,16 @@ $(document).ready(function() {
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const operator = $("input:radio[name=operator]:checked").val();
-    const result = add(number1, number2);
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
